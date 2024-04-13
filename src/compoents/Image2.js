4e31bfdb62c9afe4ee638c809bd18fe2bcb26f75
shubhampriya1 <shubhamp.ug21.ee@nitp.ac.in>
2024-04-13 22:26:01 +0530

    changes done

--- src/compoents/Image2.js
+++ src/compoents/Image2.js
@@ -1,4 +1,5 @@
-
+// import './Image2Style.css'
+import './Image2Style.css';
 import React from 'react'
 import {Link } from "react-router-dom";
 
@@ -7,28 +8,47 @@ const Image2 = () => {
     return (
       <div className="Shubham">
         <div className="mask">
-          <img className="into-img" src="https://img.freepik.com/free-photo/3d-render-technology-background-with-code-male-head_1048-5960.jpg?size=626&ext=jpg"/>
-  
+          <img
+            className="into-img"
+            src="https://img.freepik.com/free-photo/3d-render-technology-background-with-code-male-head_1048-5960.jpg?size=626&ext=jpg"
+          />
         </div>
-           <div className="content">
-       <p>MY WORK IS HERE!!!</p>
-       <h1>Projects</h1>
-      
-       <div><Link to="https://github.com/shubhampriya1/TodoList5678" className="btn">Project 1</Link>
-       <Link to="https://github.com/shubhampriya1/wheather-app" className="btn-light">Project 2</Link>
-      
-     </div>
-     <h1> 
-     <div> <Link to="https://github.com/shubhampriya1/Calculator" className="btn-light">Project 3</Link>
-       <Link to="https://github.com/shubhampriya1/wheather-app" className="btn-dark">Project 4</Link>
-</div>
-     </h1>
-     
-      
-         
+        <div className="content">
+          <p>MY WORK IS HERE!!!</p>
+          <h1>Projects</h1>
+
+          <div className="project1">
+            <Link
+              to="https://github.com/shubhampriya1/TodoList5678"
+              className="btn"
+            >
+              Project 1
+            </Link>
+            <Link
+              to="https://github.com/shubhampriya1/wheather-app"
+              className="btn-light"
+            >
+              Project 2
+            </Link>
+          </div>
+
+          <div className="project2">
+            <Link
+              to="https://github.com/shubhampriya1/Calculator"
+              className="btn-light"
+            >
+              Project 3
+            </Link>
+            <Link
+              to="https://github.com/shubhampriya1/wheather-app"
+              className="btn-dark"
+            >
+              Project 4
+            </Link>
+          </div>
+        </div>
       </div>
-      </div>
-  )
+    );
 }
 
 export default Image2
