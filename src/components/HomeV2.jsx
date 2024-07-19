926699515a014a78b60d22e97c42a1fa12e263cd
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-07-19 16:15:42 +0530

    minor change

--- src/components/HomeV2.jsx
+++ src/components/HomeV2.jsx
@@ -144,7 +144,7 @@ export default function Component() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t relative z-10">
         <p className="text-xs text-muted-foreground">
-          &copy; 2024 Jane Doe. All rights reserved.
+          &copy; 2024 Shubham Priya. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link
