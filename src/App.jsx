84f62f19e1b6673f31f7e95f1d8c2678c90e9b39
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-07-19 16:15:07 +0530

    added new home page and shad cn component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./routes/Home";
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
 import About from "./routes/About";
-
 import { Route, Routes } from "react-router-dom";
+import Component from "./components/HomeV2";
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Route path="/Project" element={<Project />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/About" element={<About />} />
+        <Route path="/new-home" element={<Component />} />
       </Routes>
     </>
   );
